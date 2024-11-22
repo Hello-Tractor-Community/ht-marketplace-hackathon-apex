a6503e2d88b0d789df7dda0a8f3ba190cb4708ad
Faraji Ombonya <farajiombonya@gmail.com>
2024-11-22 11:07:27 +0300

    feat: add more fields to tractor model

--- api/product/models.py
+++ api/product/models.py
@@ -13,7 +13,7 @@ class Image(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.url
-
+    
 
 class Tractor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -25,13 +25,22 @@ class Tractor(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     images = models.ManyToManyField(Image)
-    specifications = models.TextField()
     history = models.TextField()
     hours = models.PositiveIntegerField(default=0)
     location = models.CharField(max_length=50)
     is_approved = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     stock = models.PositiveIntegerField(default=0)
+    specifications = models.JSONField(null=True, blank=True)
+    features = models.JSONField(null=True, blank=True)
+    condition = models.CharField(max_length=50, null=True, blank=True)
+    milleage = models.PositiveIntegerField(default=0, null=True, blank=True)
+    vin = models.CharField(max_length=50)
+    transmission = models.CharField(max_length=50, null=True, blank=True)
+    drive_type = models.CharField(max_length=50, null=True, blank=True)
+    fuel_type = models.CharField(max_length=50, null=True, blank=True)
+    engine_size = models.CharField(max_length=50, null=True, blank=True)
+    offer_type = models.CharField(max_length=50, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
