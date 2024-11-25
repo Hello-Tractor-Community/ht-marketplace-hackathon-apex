570e690de51128f873df235e657f8ab4f8161458
Faraji Ombonya <farajiombonya@gmail.com>
2024-11-25 12:21:29 +0300

    update settings

--- api/PROJECT/settings.py
+++ api/PROJECT/settings.py
@@ -151,6 +151,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 # CORS
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 
