4379a56f029942e7ba3f80253126099e3ae30d14
Faraji Ombonya <farajiombonya@gmail.com>
2024-11-25 21:00:07 +0300

    feat: make name nullable

--- api/user/models.py
+++ api/user/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     firebase_id = models.CharField(max_length=100, unique=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(max_length=100)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     address = models.TextField(null=True, blank=True)
