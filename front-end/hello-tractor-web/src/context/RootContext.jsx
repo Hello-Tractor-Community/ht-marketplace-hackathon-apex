153e9cc89fc4a2ee51c99c81ee3987318adaad65
KelvinHalx <kelvinwainaina03@gmail.com>
2024-11-26 10:50:43 +0300

    Route Context

--- front-end/hello-tractor-web/src/context/RootContext.jsx
+++ front-end/hello-tractor-web/src/context/RootContext.jsx
@@ -1,11 +1,17 @@
 import PropTypes from "prop-types";
 import { ImageProvider } from "./ImageContext/ImageProvider";
 import UserProvider from "./UserContext/UserProvider";
+import SellerProvider from "./SellerContext/SellerProvider";
+import AccessProvider from "./AdminAccess/AccessProvider";
 
 const RootContext = ({ children }) => {
   return (
     <UserProvider>
-      <ImageProvider>{children}</ImageProvider>
+      <SellerProvider>
+        <ImageProvider>
+          <AccessProvider>{children}</AccessProvider>
+        </ImageProvider>
+      </SellerProvider>
     </UserProvider>
   );
 };
